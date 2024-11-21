22ee3b59ac16ece0e21333e03377bb1055293759
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-21 14:02:26 +0500

    done with rendering cartItems. items can be updated as well

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -2,13 +2,18 @@ import { Platform, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "@/providers/CartProvider";
+import { FlatList } from "react-native";
+import CartListItem from "@/components/CartListItem";
 
 const CartScreen = () => {
   const { items } = useCart();
 
   return (
     <View>
-      <Text>Cart Item length : {items.length}</Text>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+      />
 
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,25 +1,40 @@
 import { CartItem, Product } from "@/types";
+import { randomUUID } from "expo-crypto";
 import { createContext, PropsWithChildren, useContext, useState } from "react";
 
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, number: -1 | 1) => void;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
+
+// function max(a: number, b: number) {
+//   if (a > b) return a;
+//   else return b;
+// }
 
 export default function CartProvider({ children }: PropsWithChildren) {
   const [items, setItems] = useState<CartItem[]>([]);
 
   const addItem = (product: Product, size: CartItem["size"]) => {
-    // if already in cart, incremenet quantity
-    // id should be dynamic
+    // if already in cart, increment quantity
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
 
     const newCartItem: CartItem = {
-      id: "1",
+      id: randomUUID(), //now update can be done. quantity increase
       product,
       product_id: product.id,
       size,
@@ -29,10 +44,24 @@ export default function CartProvider({ children }: PropsWithChildren) {
     setItems([...items, newCartItem]);
   };
 
-  console.log(items);
+  //this is linked to the plus and minus buttons on the Cart page
+  // find the object with that id and update it
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    const updatedItems = items
+      .map((item) =>
+        item.id !== itemId
+          ? item
+          : { ...item, quantity: item.quantity + amount }
+      )
+      .filter((item) => item.quantity > 0);
+
+    setItems(updatedItems);
+  };
+
+  //   console.log(items);
 
   return (
-    <CartContext.Provider value={{ items, addItem }}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
       {children}
     </CartContext.Provider>
   );
