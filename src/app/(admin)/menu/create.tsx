571d36dd9e1cac7e7b32ecde5bef9e6a6dc16cd4
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-23 10:38:06 +0500

    added image picker. add product form is ready

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Image, StyleSheet, Text, TextInput, View } from "react-native";
 import React, { useState } from "react";
 import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import Colors from "@/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
 
 const CreateProductScreen = () => {
   const [name, setName] = useState("");
@@ -41,8 +44,30 @@ const CreateProductScreen = () => {
     return true;
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
+
       <Text style={styles.label}>Name</Text>
       <TextInput
         value={name}
@@ -81,9 +106,20 @@ const styles = StyleSheet.create({
     marginTop: 5,
     marginBottom: 20,
   },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
+  },
   label: {
     color: "gray",
     fontSize: 16,
     paddingHorizontal: 5,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
   },
 });
