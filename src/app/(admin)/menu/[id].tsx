f15162fb78b4acdfc07afb41cd494ab6956cac81
Abdul Rahim <k213951@nu.edu.pk>
2024-12-02 19:20:11 +0500

    read operation done

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -3,23 +3,30 @@ import ProductListItem, {
   defaultPizzaImage,
 } from "@/components/ProductListItem";
 import { CartItem, PizzaSize } from "@/types";
-import products from "@assets/data/products";
 import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import React, { useState } from "react";
-import { Image, Text, View, StyleSheet, Pressable } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import { useCart } from "@/providers/CartProvider"; //this gives you access to the addItem function
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useColorScheme } from "@components/useColorScheme";
 import Colors from "@/constants/Colors";
+import { useProduct } from "@/api/products";
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const { id } = useLocalSearchParams();
+  const { id: idString } = useLocalSearchParams();
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
+  const { data: product, error, isLoading } = useProduct(id);
+
   const { addItem } = useCart();
   const colorScheme = useColorScheme();
-
-  const index = Number(id);
-  const product = products[index - 1];
   const router = useRouter();
 
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
@@ -30,6 +37,10 @@ const ProductDetailsScreen = () => {
     addItem(product, selectedSize);
     router.push("/cart");
   };
+
+  if (isLoading) return <ActivityIndicator />;
+
+  if (error) return <Text>Failed to Fetch Products</Text>;
 
   return (
     <View style={styles.container}>
