92a86375206ab87f2928d7925ddd0ead14a07fac
Abdul Rahim <k213951@nu.edu.pk>
2024-12-02 18:30:30 +0500

    configured the INIT for react query - state mgmnt tool

--- src/app/(users)/menu/index.tsx
+++ src/app/(users)/menu/index.tsx
@@ -1,10 +1,20 @@
 import { FlatList, StyleSheet, View } from "react-native";
 import products from "@assets/data/products";
 import ProductListItem from "@components/ProductListItem";
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
 
 // keep structure and style seperate
 
 export default function MenuScreen() {
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data, error } = await supabase.from("products").select("*");
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <View>
       <FlatList
