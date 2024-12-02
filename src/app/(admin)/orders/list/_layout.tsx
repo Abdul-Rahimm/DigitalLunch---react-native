efe94f84a5a20cd6b18a1f49a4bc1b4ce57830e1
Abdul Rahim <k213951@nu.edu.pk>
2024-12-02 15:41:18 +0500

    configured supabase

--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -1,5 +1,12 @@
 import { Tabs } from "expo-router";
-
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function OrderListNavigator() {
-  return <Tabs />;
+  return (
+    <SafeAreaView edges={["top"]} style={{ flex: 1, backgroundColor: "white" }}>
+      <Tabs>
+        <Tabs.Screen name="index" options={{ title: "Active" }} />
+        <Tabs.Screen name="archive" options={{ title: "Archive" }} />
+      </Tabs>
+    </SafeAreaView>
+  );
 }
