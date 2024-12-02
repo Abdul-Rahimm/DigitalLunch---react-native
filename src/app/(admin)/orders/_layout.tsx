3fb8828af64cd5ddf0eeec878f20cdea4a3d60b8
Abdul Rahim <k213951@nu.edu.pk>
2024-12-02 16:27:06 +0500

    signup auth also done

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -10,7 +10,7 @@ const Menu = () => {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Orders" }} />
+      {/* <Stack.Screen name="index" options={{ title: "Orders" }} /> */}
       <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   );
