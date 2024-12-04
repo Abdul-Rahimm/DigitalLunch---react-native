6a344a3f1f5bad1af691f959ded136f8a9bf7af1
Abdul Rahim <k213951@nu.edu.pk>
2024-12-04 17:11:15 +0500

    admin is able to fetch all orders. active and archived

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,8 +1,14 @@
-import { FlatList, Text } from "react-native";
-import orders from "@assets/data/orders";
+import { ActivityIndicator, FlatList, Text } from "react-native";
 import OrderListItem from "@/components/OrderListItem";
+import { useFetchAdminOrderList } from "@/api/orders";
 
 export default function OrderScreen() {
+  const { data: orders, isLoading, error } = useFetchAdminOrderList();
+
+  if (isLoading) return <ActivityIndicator />;
+
+  if (error) return <Text>Failed to fetch admin orders all</Text>;
+
   return (
     <FlatList
       data={orders}
