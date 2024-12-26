733011c3435f8b708e015178d55d44a0d32869fd
Abdul-Rahimm <k213951@nu.edu.pk>
2024-12-26 22:44:44 +0500

    working on real-time updates on admin side. -->subscription. Update admin as soon as order rcvd

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,6 +1,8 @@
 import { ActivityIndicator, FlatList, Text } from "react-native";
 import OrderListItem from "@/components/OrderListItem";
 import { useFetchAdminOrderList } from "@/api/orders";
+import { useEffect } from "react";
+import { supabase } from "@/lib/supabase";
 
 export default function OrderScreen() {
   const {
@@ -8,6 +10,19 @@ export default function OrderScreen() {
     isLoading,
     error,
   } = useFetchAdminOrderList({ archived: false });
+
+  useEffect(() => {
+    const orders = supabase
+      .channel("custom-insert-channel")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "orders" },
+        (payload) => {
+          console.log("Change received!", payload);
+        }
+      )
+      .subscribe();
+  }, []);
 
   if (isLoading) return <ActivityIndicator />;
 
